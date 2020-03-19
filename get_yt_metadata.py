f81dcc3a44a36bc8a13430600ce690cf6962f591
akki2825 <akhilesh.rocking@gmail.com>
2020-03-19 20:39:01 +0530

    fix errors

--- get_yt_metadata.py
+++ get_yt_metadata.py
@@ -1,5 +1,5 @@
 #from get_channels import get_yt_channels
-from get_yt_data import get_data
+#from get_yt_data import get_data
 import youtube_dl
 
 import pandas as pd
@@ -10,10 +10,11 @@ ydl = youtube_dl.YoutubeDL()
 
 if __name__ == "__main__":
     data = None
-    count = 5
-    for channel in yt_data["channels"][count:]:
+    count = 0
+    for channel in yt_data["channels"]:
         pd = pd.DataFrame()
         with ydl:
             data = ydl.extract_info(channel, download=False)
-        pd_data = pd.DataFrame.from_records(data['entries'])
-        pd_data.to_csv(count + ".csv")
+        pd_data = pd.from_records(data['entries'])
+        pd_data.to_csv(str(count) + ".csv")
+        count+=1
