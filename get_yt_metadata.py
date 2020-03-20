f247482cbef12ad1f608116402aeb5d7a5293f21
Akhilesh <akhilesh.rocking@gmail.com>
2020-03-20 11:07:10 +0530

    update

--- get_yt_metadata.py
+++ get_yt_metadata.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     data = None
     count = 0
     for channel in yt_data["channels"]:
-        pd = pd.DataFrame()
+        df = pd.DataFrame()
         with ydl:
             data = ydl.extract_info(channel, download=False)
-        pd_data = pd.from_records(data['entries'])
+        pd_data = df.from_records(data['entries'])
         pd_data.to_csv(str(count) + ".csv")
         count+=1
