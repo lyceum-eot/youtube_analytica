7d6821bc8a2a9cc6e4d7820bb8178ed51f78247d
nikhilkanta <nikhilkantz25@gmail.com>
2020-03-27 23:16:57 +0530

    ignore errors in yt-dl added

--- get_yt_metadata.py
+++ get_yt_metadata.py
@@ -2,9 +2,13 @@ import youtube_dl
 
 import pandas as pd
 
+#set args for youtube-dl, added ignore errors to continue after error
+ytargs = {'ignoreerrors': True}
+
+#get the youtube channels list that need to be downloaded
 yt_data = pd.read_csv("youtube_channels_list.csv")
 
-ydl = youtube_dl.YoutubeDL()
+ydl = youtube_dl.YoutubeDL(ytopts)
 
 if __name__ == "__main__":
     data = None
@@ -13,6 +17,7 @@ if __name__ == "__main__":
         df = pd.DataFrame()
         with ydl:
             data = ydl.extract_info(channel, download=False)
-        pd_data = df.from_records(data['entries'])
-        pd_data.to_csv(str(count) + ".csv")
+        entry = [i for i in data['entries'] if i] # remove any none entries
+        pd_data = df.from_records(entry, exclude={'formats'}) # load pd.DataFrame
+        pd_data.to_csv(str(count) + ".csv") 
         count+=1
